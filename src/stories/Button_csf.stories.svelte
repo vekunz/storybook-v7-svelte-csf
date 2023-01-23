efdcc71d2973ca1d26e459e37e963b1598925df5
Veit Kunz <git@veit-kunz.de>
2023-01-23 12:05:41 +0100

    Add Svelte Story

---
+++ src/stories/Button_csf.stories.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  import Button from './Button.svelte';
+</script>
+
+<Meta
+  title="Example/Button CSF"
+  component={Button}
+  argTypes={{
+    backgroundColor: { control: 'color' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+  }}
+/>
+
+<Template let:args>
+  <Button {...args} />
+</Template>
+
+<Story name="Primary" args={{primary: true, label: 'Button'}} />
