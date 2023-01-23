f6b7536f152ae2243b303dc235f835adba0eda82
Veit Kunz <git@veit-kunz.de>
2023-01-23 11:59:20 +0100

    Add storybook csf

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -2,12 +2,13 @@ const path = require('path');
 module.exports = {
   "stories": [
     "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-svelte-csf"
   ],
   "framework": {
     "name": "@storybook/sveltekit",
